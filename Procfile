5277a49a954d75e9199e75c1d99df8ab5a0002a9
notbittu <ankitsen.in@gmail.com>
2025-04-19 19:49:11 +0530

    Simplify deployment configuration and remove unnecessary dependencies

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --worker-class gevent --workers 1 --threads 8 app:app
+web: python app.py
--- app.py
+++ app.py
@@ -5,4 +5,4 @@ import os
 if __name__ == "__main__":
     # Get port from environment variable or use 5000 as default
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)

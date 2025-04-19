2a20e713f4fa163b03064d9b983c876d80f2e353
notbittu <ankitsen.in@gmail.com>
2025-04-19 19:59:02 +0530

    Update to clean, working configuration for both local and production

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ app.debug = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
 if __name__ == "__main__":
     # For local development
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=app.debug)
+    app.run(host="0.0.0.0", port=port)
